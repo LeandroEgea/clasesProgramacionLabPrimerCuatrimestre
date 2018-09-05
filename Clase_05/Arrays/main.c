d459c1b6c4dd9418a87212b5f35e0c229889864c
alumno <alumno@LAB00PC00>
2018-09-04 21:47:09 -0300

    finalizacion del ejercicio

--- Clase_05/Arrays/main.c
+++ Clase_05/Arrays/main.c
@@ -4,57 +4,99 @@
 #include "utnmath.h"
 #define CANTIDAD 4
 
+void cargarListado(int listado[], int tamanio);
+void MostrarListado(int listado[], int tamanio);
+void MostrarInformacionDelArray(int listado[], int tamanio);
+int retornarMayor(int listado[], int tamanio);
+int retornarMenor(int listado[], int tamanio);
+int retornarCantidadDePares(int listado[], int tamanio);
 int retornarTotal(int listado[], int tamanio);
 float retornarPromedio(int listado[], int tamanio);
+
 int main()
 {
-    int numeroIngresado;
     int listadoDeNotas[CANTIDAD];
-    int i;
-    int notaMayor;
-    int notaMenor;
-    int acumuladorNotas;
-    float promedioNotas = 0;
-/*
-    printf("tamaño: %lu\n", sizeof(numeroIngresado));
-    printf("tamaño: %lu\n", sizeof(listadoDeNotas));
-    printf("direccion: %p\n", &numeroIngresado);
-    printf("direccion: %p\n", listadoDeNotas);*/
-    for(i=0;i<CANTIDAD;i++)
-    {
-        //printf("\n direccion [%d]: %p", i,&listadoDeNotas[i]);
-        printf("\nIngrese Nota %d: ", i+1);
-        scanf("%d", &numeroIngresado);
-        listadoDeNotas[i]=numeroIngresado;
-    }
-    for(i=0;i<CANTIDAD;i++)
-    {
-        printf("\nLa nota %d es :%d ", i+1, listadoDeNotas[i]);
-    }
-
-    for(i=0;i<CANTIDAD;i++)
-    {
-        if(i==0 || listadoDeNotas[i]>notaMayor)
-        {
-            notaMayor = listadoDeNotas[i];
-        }
-    }
-    for(i=0;i<CANTIDAD;i++)
-    {
-        if(i==0 || listadoDeNotas[i]<notaMenor)
-        {
-            notaMenor = listadoDeNotas[i];
-        }
-    }
-    acumuladorNotas = retornarTotal(listadoDeNotas, CANTIDAD);
-    promedioNotas = retornarPromedio(listadoDeNotas, CANTIDAD);
-    printf("\nLa nota total es: %d", acumuladorNotas);
-    printf("\nLa nota promedio es: %.2f", promedioNotas);
-    printf("\nLa nota mayor es: %d", notaMayor);
-    printf("\nLa nota menor es: %d", notaMenor);
+    cargarListado(listadoDeNotas, CANTIDAD);
+    MostrarListado(listadoDeNotas, CANTIDAD);
+    MostrarInformacionDelArray(listadoDeNotas, CANTIDAD);
     return 0;
 }
-
+void cargarListado(int listado[], int tamanio)
+{
+    int i;
+    int numeroIngresado;
+    for(i=0;i<tamanio;i++)
+    {
+        printf("\nIngrese numero %d: ", i+1);
+        scanf("%d", &numeroIngresado);
+        listado[i]=numeroIngresado;
+    }
+}
+void MostrarListado(int listado[], int tamanio)
+{
+    int i;
+    for(i=0;i<tamanio;i++)
+    {
+        printf("\nLa nota %d es :%d ", i+1, listado[i]);
+    }
+}
+void MostrarInformacionDelArray(int listado[], int tamanio)
+{
+    int notaMayor;
+    int notaMenor;
+    int cantidadDePares;
+    int acumuladorNotas;
+    float promedioNotas;
+    notaMayor = retornarMayor(listado, tamanio);
+    notaMenor = retornarMenor(listado, tamanio);
+    cantidadDePares = retornarCantidadDePares(listado, tamanio);
+    acumuladorNotas = retornarTotal(listado, tamanio);
+    promedioNotas = retornarPromedio(listado, tamanio);
+    printf("\nLa nota mayor es: %d", notaMayor);
+    printf("\nLa nota menor es: %d", notaMenor);
+    printf("\nLa cantidad de pares es: %d", cantidadDePares);
+    printf("\nLa nota total es: %d", acumuladorNotas);
+    printf("\nLa nota promedio es: %.2f", promedioNotas);
+}
+int retornarMayor(int listado[], int tamanio)
+{
+    int mayor;
+    int i;
+    for(i=0;i<tamanio;i++)
+    {
+        if(i==0 || listado[i]>mayor)
+        {
+            mayor = listado[i];
+        }
+    }
+    return mayor;
+}
+int retornarMenor(int listado[], int tamanio)
+{
+    int menor;
+    int i;
+    for(i=0;i<tamanio;i++)
+    {
+        if(i==0 || listado[i]<menor)
+        {
+            menor = listado[i];
+        }
+    }
+    return menor;
+}
+int retornarCantidadDePares(int listado[], int tamanio)
+{
+    int i;
+    int cantidadDePares = 0;
+    for(i=0;i<tamanio;i++)
+    {
+        if(listado[i] % 2 == 0)
+        {
+            cantidadDePares++;
+        }
+    }
+    return cantidadDePares;
+}
 int retornarTotal(int listado[], int tamanio)
 {
     int i;
